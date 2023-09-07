f478bb950558b7d369eac6469c22dcf7c959ac87
Edward Betts <edward@4angle.com>
2023-09-07 20:19:41 +0100

    New upstream version 1.95.2

---
+++ tests/test_aio_multi_flags.py
@@ -0,0 +1,22 @@
+import pytest
+
+from dbus_fast.aio import MessageBus
+from dbus_fast.service import ServiceInterface, method
+
+
+@pytest.mark.asyncio
+async def test_multiple_flags_in_message():
+    class ExampleInterface(ServiceInterface):
+        def __init__(self, name):
+            super().__init__(name)
+
+        @method()
+        def Echo(self, what: "s") -> "s":  # noqa: F821
+            return what
+
+    bus = await MessageBus().connect()
+    interface = ExampleInterface("test.interface")
+    bus.export("/test/path", interface)
+    await bus.request_name("test.name")
+    bus.disconnect()
+    await bus.wait_for_disconnect()
